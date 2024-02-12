5cd2f29624c9610dabc256ac95d4f333e4f9877f
preeti tambe <preetitambe158@gmail.comgit config --global user.name priti>
2024-02-12 15:53:39 +0530

    Created One POJO Class and One EmpDao class

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,37 @@
 
 public class Employee {
+	private int id;
+	private String name,pass,email,con;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCon() {
+		return con;
+	}
+	public void setCon(String con) {
+		this.con = con;
+	}
+	
 
 }
